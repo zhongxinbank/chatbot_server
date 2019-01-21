dbeb9f90735ffbec17030fd0b060b619573b4e86
yym6472 <yanyuanmeng@bupt.edu.cn>
2019-01-21 13:07:49 +0800

    added the chatbot module to obtain response for specific user_input

---
+++ chatbot/NLG/nlg_rule.py
@@ -0,0 +1,143 @@
+# Created by Helic on 2017/7/21
+import json
+
+
+class NLG:
+
+    def __init__(self):
+        # 读取dataset
+        with open('chatbot/NLG/data/nlg_template.json', encoding='utf-8') as f:
+            value = json.load(f)
+            self.request = value['request']  # list
+            self.inform = value['inform']
+            self.select = value['select']
+            self.greeting = value['greeting']
+            self.bye = value['bye']
+
+    def get_sentence(self, diaact):
+        request_slots = []
+        inform_slots = []
+        slot_val = []
+        num = []
+        s = {}
+        for i in diaact['request_slots']:
+            request_slots.append(i)
+        for i in diaact['inform_slots']:
+            inform_slots.append(i)
+        # print(request_slots, inform_slots)
+        # print(diaact['inform_slots'][inform_slots[0]])
+        if inform_slots != []:
+            if diaact['inform_slots'][inform_slots[0]] != 'UNK':
+                slot_val = diaact['inform_slots'][inform_slots[0]]
+                # print(slot_val)
+        if diaact["diaact"] == 'request':
+            for dic in self.request:
+                if dic['request_slots'] == request_slots and dic['inform_slots'] == inform_slots:
+                    if slot_val == []:
+                        s = dic['nl']['agt']
+                    else:
+                        s = dic['nl']['agt']
+                        for i in s:
+                            sentence = s[i]
+                            sentence = sentence.replace('$slot_val$', slot_val)
+                            s[i] = sentence
+                        num = []
+                    return s
+        elif diaact["diaact"] == 'inform':
+            for dic in self.inform:
+                if dic['inform_slots'] == inform_slots:
+                    if slot_val == []:
+                        s = dic['nl']['agt']
+                    else:
+                        s = dic['nl']['agt']
+                        for i in s:
+                            sentence = s[i]
+                            sentence = sentence.replace('$slot_val$', slot_val)
+                            s[i] = sentence
+                        num = []
+                    return s
+        elif diaact["diaact"] == 'select':
+            for dic in self.select:
+                # print(dic['inform_slots'])
+                if dic['inform_slots'] == inform_slots:
+                    if slot_val == []:
+                        s = dic['nl']['agt']
+                    else:
+                        s = dic['nl']['agt']
+                        for i in s:
+                            sentence = s[i]
+                            sentence = sentence.replace('$slot_val$', slot_val)
+                            s[i] = sentence
+                        num = []
+                    return s
+        elif diaact["diaact"] == 'greeting':
+            return self.greeting[0]['nl']['agt']
+        elif diaact["diaact"] == 'bye':
+            return self.bye[0]['nl']['agt']
+        else:
+            return diaact
+
+
+
+'''
+dia_act = {'diaact': 'request',
+           'request_slots': {'child_name': 'UNK'},
+           'inform_slots': {}
+          }
+dia_act = {'diaact': 'request',
+           'request_slots': {'child_name':'UNK'},
+           'inform_slots': {'fee':'UNK'}
+        }
+dia_act = {'diaact': 'request',
+           'request_slots': {'reserve_time':'UNK'},
+           'inform_slots': {'school_phone':'一二三四五'}
+        }
+dia_act = {'diaact': 'inform',
+           'request_slots': {},
+           'inform_slots': {'fee':'UNK'}
+        }
+
+dia_act = {'diaact': 'inform',
+           'request_slots': {},
+           'inform_slots': {'school_phone':'一二三四五'}
+        }
+dia_act = {'diaact': 'select',
+           'request_slots': {'reserve_location': 'UNK'},
+           'inform_slots': {'reserve_location': '一，二，三'}
+        }
+dia_act = {'diaact': 'greeting',
+           'request_slots':{},
+           'inform_slots':{}
+        }
+dia_act = {'diaact': 'bye',
+           'request_slots':{},
+           'inform_slots':{}
+        }
+
+'''
+
+# nlg = NLG()
+# dia_act = {'diaact': 'request',
+#            'request_slots': {'child_name': 'UNK'},
+#            'inform_slots': {}
+#           }
+# s = nlg.get_sentence(dia_act)
+# print(s)
+
+
+
+
+'''
+self.request_slots = ['school_location', 'school_phone', 'sale', 'other_contact', 'cut_in', 'class_schedule',
+                              'have_school_somewhere', 'attend_class_alone', 'allow_audition', 'audition_free',
+                              'child_attend', 'allow_parents_together', 'class_length', 'audition_introduction',
+                              'textbook', 'fulltime_or_sparetime', 'class_size', 'length_of_per_period',
+                              'allow_return_premium', 'lesson_accompany', 'school_type', 'teacher_nation', 'class_type',
+                              'online_course', 'online_course_location', 'fee', 'ruisi_introduction']
+
+
+# agent需要去问用户的slot
+self.agent_request_slots = ["child_age", "english_level", "special_need", "know_about_ruisi", "client_location",
+                            "phone_number", "client_name", "client_gender",
+                            "child_name"] # "reserve_location", "reserve_time"
+'''
--- responders/probe_responder.py
+++ responders/probe_responder.py
@@ -4,17 +4,19 @@ from constant import *
 from constant import RequestException, AppException, DatabaseException
 from database.dao.session_dao import SessionDAO
 
+from chatbot.DialogManager.DialogManager import DM
+
 
 class ProbeResponder(object):
 
     def on_get(self, req, res):
-        # 此处需要对请求参数做校验
+        # TODO: CHECK FOR PARAMS `user_id`
         try:
             user_id = req.params.get("user_id")
             if user_id is not None:
-                # 此处初始化一个sesssion,存入redis,返回session_id
+                new_session = DM()
                 session_id = str(uuid.uuid1())
-                SessionDAO.set_user_session(session_id, "test", 600)  # 此处需要修改成DM instance
+                SessionDAO.set_user_session(session_id, new_session, 600)
                 res.status = falcon.HTTP_200
                 res.body = json.dumps({
                     'session_id': session_id,
@@ -25,7 +27,7 @@ class ProbeResponder(object):
                 }, ensure_ascii=False)
                 return res
             else:
-                raise RequestException(500, 'No suffient params in request url')   # 传参错误统一返回500
+                raise RequestException(500, 'No suffient params in request url')
         except (AppException, RequestException, DatabaseException) as error:
             res.status = falcon.HTTP_200
             res.body = json.dumps({ 
@@ -47,19 +49,28 @@ class ProbeResponder(object):
             return res
         
     def on_post(self, req, res):
-        try: 
+        try:
             req_body = json.loads(req.stream.read())
             session_id = req_body.get('session_id')
             user_input = req_body.get('user_input')
             if session_id is not None and SessionDAO.get_user_session(session_id) and user_input is not None:
                 session = SessionDAO.get_user_session(session_id)
-                output_message = user_input  # DM执行返回的结果 TODO  对话结束时要直接kill session
+                agent_response_diaact, index = session.agent_response(user_input)
+                try:
+                    output_message = session.agent_nl(agent_response_diaact, index)
+                except KeyError as e:
+                    output_message = "家长可以留下您的联系方式哦，我们后续会安排专业的老师与您对接，为您详细介绍相关事宜。"
+                if session.flag:
+                    SessionDAO.kill_user_session(session_id)
+                else:
+                    SessionDAO.set_user_session(session_id, session, 600)
+                # TODO: RECORD QA CONTEXT TO DISK
                 res.status = falcon.HTTP_200
                 res.body = json.dumps({ 
                     'output': output_message,
                     'status': {
                         'code': 200,
-                        'message': "执行成功"
+                        'message': "Successfully produced the response"
                     }
                 }, ensure_ascii=False)
                 return res
